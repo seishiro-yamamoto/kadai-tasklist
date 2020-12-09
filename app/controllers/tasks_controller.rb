66f8fd3dbe6795fd214c180d9e67923e1116f254
seishiro.yamamoto <yamamotoseishiro@gmail.com>
2020-12-09 07:37:18 +0000

    modified3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
 
-   before_action :require_user_logged_in, only:[:index, :create, :destroy]
+   before_action :require_user_logged_in, only:[:index, :show, :new, :edit, :update, :create, :destroy]
    before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'users/new'
-  get 'users/create'
   root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
   
   get 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
