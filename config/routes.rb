e41770049da8061feb2455fbca0edc8179391f8a
seishiro.yamamoto <yamamotoseishiro@gmail.com>
2020-12-11 11:28:32 +0000

    modifiedA

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'login', to: 'sessions#new'
